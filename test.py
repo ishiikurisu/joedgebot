9a8f4683303db92dadd862812f717023b0ed3292
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-07-16 01:05:21 -0300

    Refactoring tests

--- test.py
+++ test.py
@@ -2,24 +2,26 @@ import unittest
 import judge
 
 class TestJudge(unittest.TestCase):
+    def setUp(self):
+        self.script = 'print(input())'
+        self.txt = 'hello joe!'
+        self.src_script = 'hello.py'
+        self.src_txt = 'hello.txt'
+
     def test_try_to_open_inexistent_file(self):
         with self.assertRaises(FileNotFoundError):
             self.judge = judge.Judge('randomscript', 'randominlet')
 
     def test_try_to_save_to_file_and_delete_afterwards(self):
-        script = 'print(input())'
-        txt = 'hello joe!'
-        src_script = 'hello.py'
-        src_txt = 'hello.txt'
-        judge.Judge.save(src_script, script)
-        judge.Judge.save(src_txt, txt)
-        self.judge = judge.Judge(src_script, src_txt)
+        judge.Judge.save(self.src_script, self.script)
+        judge.Judge.save(self.src_txt, self.txt)
+        self.judge = judge.Judge(self.src_script, self.src_txt)
         self.assertIsNotNone(self.judge.script)
         self.assertIsNotNone(self.judge.txt)
-        judge.Judge.delete(src_script)
-        judge.Judge.delete(src_txt)
+        judge.Judge.delete(self.src_script)
+        judge.Judge.delete(self.src_txt)
         with self.assertRaises(FileNotFoundError):
-            self.judge = judge.Judge(src_script, src_txt)
+            self.judge = judge.Judge(self.src_script, self.src_txt)
 
 if __name__ == '__main__':
     unittest.main()
